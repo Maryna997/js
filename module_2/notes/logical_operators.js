264b5ad8d50678e16c517f2fb50455b24073f90f
Maryna Zuikova <mzujkova2002@gmail.com>
2025-03-28 21:42:27 +0000

    add module 2

---
+++ module_2/notes/logical_operators.js
@@ -0,0 +1,6 @@
+console.log(Boolean(NaN)); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(0)); // false
+console.log(Boolean(3.14)); // true
+console.log(Boolean(-10)); // true
